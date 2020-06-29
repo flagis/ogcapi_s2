a193d7c29e0948e2255f501ac870065d954c3f73
lathoub <4082369+lathoub@users.noreply.github.com>
2020-06-29 21:22:11 +0200

    Update index.js

--- step7/index.js
+++ step7/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 80
 
-var birds = require('./kontich')
+var kontich = require('./kontich')
 
-app.use('/kontich', birds)
+app.use('/kontich', kontich)
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
