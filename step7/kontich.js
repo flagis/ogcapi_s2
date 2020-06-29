fd876bd1784c14dd6f29a9a693006a68c924ce8d
lathoub <4082369+lathoub@users.noreply.github.com>
2020-06-29 21:12:02 +0200

    Update kontich.js

--- step7/kontich.js
+++ step7/kontich.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var router = express.Router()
-   
+var url = require('url');
+var landingPage = require('./data/landingPage');
+var path = require('path');
+
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now())
@@ -10,50 +13,15 @@ router.use(function timeLog (req, res, next) {
 // define the home page route
 router.get('/', function (req, res) {
 
-  var landingPage = {};
-  landingPage.title = "Kontich";
-  landingPage.description = "Access to data about buildings in the city of Kontich via a Web API that conforms to the OGC API Features specification.";
-  landingPage.links = [];
-  
-  var item = {};
-  item.href = "http://localhost/kontich/";
-  item.rel = "self";
-  item.type = "application/json";
-  item.title = "this document";
-  landingPage.links.push(item)
-
-  // The API definition is metadata about the API and strictly not part of the
-  // API itself, but it MAY be hosted as a sub-resource to the base path of the
-  // API, for example, at path /api. There is no need to include the path of the 
-  // API definition in the API definition itself.
-  var item = {};
-  item.href = "http://localhost/kontich/api";
-  item.rel = "service-desc";
-  item.type = "application/vnd.oai.openapi+json;version=3.0";
-  item.title = "the API definition";
-  landingPage.links.push(item)
-  var item = {};
-  item.href = "http://localhost/kontich/api.html";
-  item.rel = "service-doc";
-  item.type = "text/html";
-  item.title = "the API documentation";
-  landingPage.links.push(item)
-
-  var item = {};
-  item.href = "http://localhost/kontich/conformance";
-  item.rel = "conformance";
-  item.type = "application/json";
-  item.title = "OGC API conformance classes implemented by this server";
-  landingPage.links.push(item)
-  
-  var item = {};
-  item.href = "http://localhost/kontich/collections";
-  item.rel = "data";
-  item.type = "application/json";
-  item.title = "Information about the feature collections";
-  landingPage.links.push(item)
-
-  res.json(landingPage)
+  var urlParts = url.parse(req.url, true);
+  if (null == urlParts.query.f)
+    res.json(landingPage)
+  else if ("json" == urlParts.query.f)
+    res.json(landingPage)
+  else if ("html" == urlParts.query.f)
+    res.sendFile(path.join(__dirname + '/data/landingPage.html'));
+  else
+    res.json(400, "{'code': 'InvalidParameterValue', 'description': 'Invalid format'}")
 })
 
 // The server SHALL support the HTTP GET operation at the path /conformance.
